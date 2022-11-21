42781bbcc11be356bc514bfdd38303a47f4c9d08
Prasad Honrao <honrao.prasad@gmail.com>
2022-11-21 07:04:12 +0000

    Add factorial - csharp program

---
+++ factorial/cshap/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+var input = Int32.Parse(Console.ReadLine());
+
+int userInput = input;
+long factorial = 1;
+
+while (input > 0) {
+    factorial = factorial * input;
+    input = input - 1;
+}
+
+Console.WriteLine("Factorial of " + userInput + " is " + factorial);
