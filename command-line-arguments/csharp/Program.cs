89c1a037d179d63b158c26efb6d38e373a26b565
Prasad Honrao <honrao.prasad@gmail.com>
2022-11-22 07:33:17 +0000

    Remove warnings from csharp programs

--- command-line-arguments/csharp/Program.cs
+++ command-line-arguments/csharp/Program.cs
@@ -1,4 +1,6 @@
-﻿// Note: args is a argument to main function which is been abstracted by higher level statements
+﻿using System;
+
+// Note: args is a argument to main function which is been abstracted by higher level statements
 if (args.Length == 0) {
     Console.WriteLine("No command line arguments were specified");
 }
